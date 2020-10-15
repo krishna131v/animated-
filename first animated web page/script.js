dede9fd55bec40e23ed7483627057b76340b9291
krishna131v <72179263+krishna131v@users.noreply.github.com>
2020-10-15 18:25:54 +0530

    Add files via upload

---
+++ first animated web page/script.js
@@ -0,0 +1,7 @@
+var animation = bodymovin.loadAnimation({
+    container: document.getElementById('anim'),
+    render: 'svg',
+    loop:true,
+    autoplay:true,
+    pat:'24317-paper-plane.json'
+})
